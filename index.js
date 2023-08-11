34e76f031b9a52b68a491cc1d0f3cc65c14288b5
Gisele Ribeiro <giselesousar@gmail.com>
2023-08-11 11:04:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-import links from './links.json' assert { type: "json" };;
+window.onload = async function () {
+    const response = await fetch("./links.jsonn");
+    const data = await response.json();
+    
+    const container = document.getElementById('links');
 
-const container = document.getElementById('links');
-
-for (const link of links) {
-    const card = document.createElement('a');
-    card.classList.add("card");
-    card.appendChild(document.createTextNode(link.nome));
-    if(link.url) {
-        if(link.url.includes("gmail")){
-            card.setAttribute("href", "mailto:" + link.url);
-        } else {
-            card.setAttribute("href", link.url);   
+    for (const link of links) {
+        const card = document.createElement('a');
+        card.classList.add("card");
+        card.appendChild(document.createTextNode(link.nome));
+        if(link.url) {
+            if(link.url.includes("gmail")){
+                card.setAttribute("href", "mailto:" + link.url);
+            } else {
+                card.setAttribute("href", link.url);   
+            }
         }
+        card.setAttribute("target", "_blank");
+        container.appendChild(card)
     }
-    card.setAttribute("target", "_blank");
-    container.appendChild(card)
 }
