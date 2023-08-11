1d1ccfd0ac7ffae982c4873aa441970a9d8be8d0
Gisele Ribeiro <giselesousar@gmail.com>
2023-08-11 09:22:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,18 +2,12 @@ import links from './links.json' assert { type: "json" };;
 
 const container = document.getElementById('links');
 
-function onClick(link){
-    window.open(link);
-}
-
 for (const link of links) {
-    const card = document.createElement('div');
+    const card = document.createElement('a');
     card.classList.add("card");
-    const a = document.createElement('a');
-    a.appendChild(document.createTextNode(link.nome));
+    card.appendChild(document.createTextNode(link.nome));
     if(link.url) {
-        a.setAttribute("href", link.url);   
+        card.setAttribute("href", link.url);   
     }
-    card.appendChild(span);
     container.appendChild(card)
 }
