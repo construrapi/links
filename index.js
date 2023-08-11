cab56b83af71b9334b390fadccf5330662a217f3
Gisele Ribeiro <giselesousar@gmail.com>
2023-08-11 10:57:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ for (const link of links) {
     card.classList.add("card");
     card.appendChild(document.createTextNode(link.nome));
     if(link.url) {
-        card.setAttribute("href", link.url);   
+        if(link.url.includes("gmail")){
+            card.setAttribute("href", "mailto:" + link.url);
+        } else {
+            card.setAttribute("href", link.url);   
+        }
     }
     card.setAttribute("target", "_blank");
     container.appendChild(card)
